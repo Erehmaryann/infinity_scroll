021201bc86548dd0a4b80c03dc87aa0e69b5f5a3
nugi-developers <68599801+Erehmaryann@users.noreply.github.com>
2021-08-09 16:08:53 -0700

    set up the try and catch block for the api request

---
+++ script.js
@@ -0,0 +1,10 @@
+//Unsplash API
+const count = 10;
+const apiKey = "RYnkyRnjFT192RS26aNbAiAl8xDkz6G6DaRoDcIVl_c";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get random photo from Unsplash API
+async function getRandomPhoto() {
+  try {
+  } catch (e) {}
+}
