9a31fc57619023ffc798ae8fd256f4c2d88bbb25
nugi-developers <68599801+Erehmaryann@users.noreply.github.com>
2021-08-10 16:09:25 -0700

    wrote a helper function to set attributes

--- script.js
+++ script.js
@@ -9,19 +9,35 @@ const count = 10;
 const apiKey = "RYnkyRnjFT192RS26aNbAiAl8xDkz6G6DaRoDcIVl_c";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Helper function to setAttributes on an element
+function setAttributes(element, attributes) {
+  for (let key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
 // Create Elements for links and photos, Add to DOM
 function displayPhotos() {
   // Run function for each obj in photosArray
   photosArray.forEach((photo) => {
     // Create a link to unsplash
     const link = document.createElement("a");
-    link.setAttribute("href", photo.links.html);
-    link.target = "_blank";
+    // link.setAttribute("href", photo.links.html);
+    // link.target = "_blank";
+    setAttributes(link, {
+      href: photo.links.html,
+      target: "_blank",
+    });
     // Create an image tag to hold the image
     const image = document.createElement("img");
-    image.src = photo.urls.regular;
-    image.setAttribute("alt", photo.alt_description);
-    image.title = photo.alt_description;
+    // image.src = photo.urls.regular;
+    // image.setAttribute("alt", photo.alt_description);
+    // image.title = photo.alt_description;
+    setAttributes(image, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
     // Put image into link, and then put both into the imageContainer
     link.appendChild(image);
     imageContainer.appendChild(link);
